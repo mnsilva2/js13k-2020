348e936a996ce9df563540a284328fb5a8a89cd0
Miguel Silva <mnsilva2@gmail.com>
2020-09-11 23:48:17 +0000

    Adds Render Text Part
    Adds Render Man in and out

--- src/levels.js
+++ src/levels.js
@@ -1,39 +1,43 @@
 module.exports = {
-    levels: [
-        {
-            papers: ["a1", "b1", "c1", "d1"],
-            text: "Welcome to your new position! Your task here is simple, organize the files to make a report.\nFind all the parts from the same topic, and join them. there are a \n Simply ",
-            time: 60,
-            required: 1
-        }
-    ],
-    loadLevel: function (currentLevel, width, height) {
+  levels: [{
+    papers: ["a1", "b1", "c1", "d1"],
+    text: "Welcome to your new position! Your task here is simple, organize the files to make a report.\nFind all the parts from the same topic, and join them.\n Simply Drag the paper parts to the to the right and the machine does the rest.\nYou have all day to make one report.",
+    time: 60,
+    required: 1
+  }],
+  //
+  loadLevel: function (currentLevel, width, height) {
 
-        const level = this.levels[currentLevel];
-        const map = { 'a': 'cover', b: 'introduction', c: 'content', d: 'conclusion' }
-        let papers = []
-        for (let i = 0; i < level.papers.length; i++) {
-            const y = Math.floor(Math.random() * height)
-            const x = Math.floor(Math.random() * width);
-
-            const paperDef = level.papers[i];
-            var paper = {
-                id: i,
-                type: map[paperDef.substr(0, 1)],
-                variant: paperDef.substr(1, 1),
-                pos: {
-                    x,
-                    y
-                },
-                ogPos: {
-                    x,
-                    y
-                },
-                rotation: Math.floor(Math.random() * 45 * 2) - 45
-            }
-            papers.push(paper)
-        }
-        return papers
+    const level = this.levels[currentLevel];
+    const map = {
+      'a': 'cover',
+      b: 'introduction',
+      c: 'content',
+      d: 'conclusion'
     }
+    let papers = []
+    for (let i = 0; i < level.papers.length; i++) {
+      const y = Math.floor(Math.random() * height)
+      const x = Math.floor(Math.random() * width);
+
+      const paperDef = level.papers[i];
+      var paper = {
+        id: i,
+        type: map[paperDef.substr(0, 1)],
+        variant: paperDef.substr(1, 1),
+        pos: {
+          x,
+          y
+        },
+        ogPos: {
+          x,
+          y
+        },
+        rotation: Math.floor(Math.random() * 45 * 2) - 45
+      }
+      papers.push(paper)
+    }
+    return papers
+  }
 
 };
--- src/render.js
+++ src/render.js
@@ -1,107 +1,162 @@
 module.exports = {
-    renderPapers: function (papers, mousedown) {
-        for (var i = 0; i < papers.length; i++) {
-            this.renderPaper(papers[i], mousedown)
-        }
-    },
-    renderPaper: function (paper, mousedown) {
-
-        const paperElem = this.createBasicPaper(paper)
-        paperElem.style.top = paper.ogPos.y + "px"
-        paperElem.style.left = paper.ogPos.x + "px"
-        paperElem.style.transform = "rotate(" + paper.rotation + "deg)";
-        paperElem.addEventListener('mousedown', (event) => { mousedown(event) })
-        document.getElementById("work-table").appendChild(paperElem);
-    },
-    renderPlaced: function (papers, callback) {
-        for (const property in papers) {
-            const parent = document.getElementById(property)
-            if (parent) {
-                if (papers[property]) {
-                    const oldChild = document.getElementById(papers[property].id)
-                    console.log(oldChild, property, papers[property].id, papers)
-                    oldChild.parentElement.removeChild(oldChild)
-                    const paperElem = this.createBasicPaper(papers[property])
-                    paperElem.addEventListener('click', callback)
-                    parent.appendChild(paperElem);
-                }
-            }
-        }
-    },
-    createBasicPaper: function (paper) {
-        const paperElem = document.createElement("div")
-        paperElem.id = paper.id
-        paperElem.innerText = paper.type
-        paperElem.className = `page ${paper.type} v-${paper.variant}`
-        return paperElem
-    },
-    addReportsToPile: function (papersPlaced) {
-        let randomX = Math.random() * 190;
-        for (const property in papersPlaced) {
-            document.getElementById(property).innerHTML = "";
-            //@TEMP IF
-            if (papersPlaced[property]) {
-                const paperElem = this.createBasicPaper(papersPlaced[property])
-                paperElem.style.position = "absolute";
-                switch (papersPlaced[property].type) {
-                    case "cover":
-                        paperElem.style.transform = "translate(20px, -150px)"
-                        break;
-                    case "introduction":
-                        paperElem.style.transform = "translate(140px, -150px)"
-                        break;
-                    case "content":
-                        paperElem.style.transform = "translate(260px, -150px)"
-                        break;
-                    case "conclusion":
-                        paperElem.style.transform = "translate(380px, -150px)"
-                        break;
-
-                }
-                setTimeout(() => {
-                    paperElem.style.transform = "translate(" + randomX + "px, -150px)"
-                    setTimeout(() => {
-                        paperElem.style.transform = "translate(" + randomX + "px, 0)"
-                    }, 250)
-                }, 50)
-                document.getElementById("final").appendChild(paperElem)
-                // document.getElementById(papersPlaced[property].id).style.transform = "translate(10px)"
-            }
-        }
-    },
-    renderTimer: function (time) {
-        let start = null
-        var element = document.getElementById("timer");
-        element.classList.remove("done")
-
-        function step(timestamp) {
-            if (element.classList.contains("done")) {
-                return
-            }
-
-            if (!start) start = timestamp;
-            var progress = timestamp - start;
-            element.style.width = 100 - ((progress * 100) / (time * 1000)) + "%";
-            if (progress < time * 1000) {
-                window.requestAnimationFrame(step);
-            }
-        }
-        window.requestAnimationFrame(step);
-    },
-    renderLevel: function (level, callback) {
-        //Add Guy
-        //Add Text
-        //When Text is done, callback
-        callback();
-    },
-    endLevel: function () {
-        //Fade.
-        //clear everything.
-        //Show next day text
-        //Fade.
-    },
-    cleanTable: function () {
-        document.getElementById("work-table").innerHTML = ""
+  renderPapers: function (papers, mousedown) {
+    for (var i = 0; i < papers.length; i++) {
+      this.renderPaper(papers[i], mousedown)
     }
+  },
+  renderPaper: function (paper, mousedown) {
+
+    const paperElem = this.createBasicPaper(paper)
+    paperElem.addEventListener('mousedown', (event) => {
+      mousedown(event)
+    })
+    paperElem.style.top = 0
+    paperElem.style.left = "100px"
+    paperElem.style.transform = "rotate(0deg) scale(1.2)";
+    document.getElementById("work-table").appendChild(paperElem);
+    paperElem.classList.add("transition")
+
+    setTimeout(() => {
+      paperElem.style.top = paper.ogPos.y + "px"
+      paperElem.style.left = paper.ogPos.x + "px"
+      paperElem.style.transform = "rotate(" + paper.rotation + "deg)";
+      setTimeout(() => {
+        paperElem.classList.remove("transition")
+      }, 500)
+    })
+  },
+  renderPlaced: function (papers, callback) {
+    for (const property in papers) {
+      const parent = document.getElementById(property)
+      if (parent) {
+        if (papers[property]) {
+          const oldChild = document.getElementById(papers[property].id)
+          console.log(oldChild, property, papers[property].id, papers)
+          oldChild.parentElement.removeChild(oldChild)
+          const paperElem = this.createBasicPaper(papers[property])
+          paperElem.addEventListener('click', callback)
+          parent.appendChild(paperElem);
+        }
+      }
+    }
+  },
+  createBasicPaper: function (paper) {
+    const paperElem = document.createElement("div")
+    paperElem.id = paper.id
+    paperElem.innerText = paper.type
+    paperElem.className = `page ${paper.type} v-${paper.variant}`
+    return paperElem
+  },
+  addReportsToPile: function (papersPlaced) {
+    let randomX = Math.random() * 190;
+    for (const property in papersPlaced) {
+      document.getElementById(property).innerHTML = "";
+      //@TEMP IF
+      if (papersPlaced[property]) {
+        const paperElem = this.createBasicPaper(papersPlaced[property])
+        paperElem.style.position = "absolute";
+        switch (papersPlaced[property].type) {
+          case "cover":
+            paperElem.style.transform = "translate(20px, -150px)"
+            break;
+          case "introduction":
+            paperElem.style.transform = "translate(140px, -150px)"
+            break;
+          case "content":
+            paperElem.style.transform = "translate(260px, -150px)"
+            break;
+          case "conclusion":
+            paperElem.style.transform = "translate(380px, -150px)"
+            break;
+
+        }
+        setTimeout(() => {
+          paperElem.style.transform = "translate(" + randomX + "px, -150px)"
+          setTimeout(() => {
+            paperElem.style.transform = "translate(" + randomX + "px, 0)"
+          }, 250)
+        }, 50)
+        document.getElementById("final").appendChild(paperElem)
+        // document.getElementById(papersPlaced[property].id).style.transform = "translate(10px)"
+      }
+    }
+  },
+  renderTimer: function (time) {
+    let start = null
+    var element = document.getElementById("timer");
+    element.classList.remove("done")
+
+    function step(timestamp) {
+      if (element.classList.contains("done")) {
+        return
+      }
+
+      if (!start) start = timestamp;
+      var progress = timestamp - start;
+      element.style.width = 100 - ((progress * 100) / (time * 1000)) + "%";
+      if (progress < time * 1000) {
+        window.requestAnimationFrame(step);
+      }
+    }
+    window.requestAnimationFrame(step);
+  },
+  renderLevel: function (level, callback) {
+    //Man Enters
+    this.renderMan(() => {
+      this.renderText(level.text, 0, () => {
+        document.getElementById("next").style.display = "none"
+        callback();
+        document.getElementById("guy").classList.remove("enter");
+        setTimeout(() => {
+          document.getElementById("guy").classList.add("leave");
+        }, 2000)
+      })
+    })
+
+  },
+  renderText: function (text, part, callback) {
+    btn = document.getElementById("next")
+    btn.style.display = "none"
+    textpart = text.split("\n")[part]
+    console.log(textpart.length, part, )
+    for (let i = 0; i < textpart.length; i++) {
+      setTimeout(() => {
+        document.getElementById("text").innerText = textpart.substring(0, i + 1)
+      }, i * 25)
+    }
+    setTimeout(() => {
+      btn.onclick = () => {
+        if (part + 1 == text.split("\n").length) {
+          callback()
+        } else {
+          this.renderText(text, part + 1, callback)
+        }
+      };
+      if (part + 1 == text.split("\n").length) {
+        btn.innerText = "Start Work!"
+      } else {
+        btn.innerText = "Continue"
+
+      }
+      btn.style.display = "block"
+
+    }, (textpart.length + 10) * 25)
+  },
+  renderMan: function (callback) {
+    document.getElementById("guy").classList.remove("leave");
+    document.getElementById("guy").classList.add("enter");
+    setTimeout(() => {
+      callback()
+    }, 2500)
+  },
+  endLevel: function () {
+    //Fade.
+    //clear everything.
+    //Show next day text
+    //Fade.
+  },
+  cleanTable: function () {
+    document.getElementById("work-table").innerHTML = ""
+  }
 
 };
